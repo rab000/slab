ee54d8afe4a13c23c622ab9d7a7c3473006bf829
rab000 <ce1985@126.com>
2020-08-23 20:36:38 +0800

    bloom完毕

--- Assets/shader/postEffect/BloomPostEffect/BloomPostEffect.cs
+++ Assets/shader/postEffect/BloomPostEffect/BloomPostEffect.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using UnityEngine;
 
+//https://catlikecoding.com/unity/tutorials/advanced-rendering/bloom/
 [ExecuteInEditMode, ImageEffectAllowedInSceneView]
 public class BloomPostEffect : MonoBehaviour
 {
@@ -14,16 +15,38 @@ public class BloomPostEffect : MonoBehaviour
     //标识第3个pass
     const int ApplyBloomPass = 3;
     //标识第4个pass
-    //const int DebugBloomPass = 4;
+    const int DebugBloomPass = 4;
 
     public Shader bloomShader;
 
     [NonSerialized] Material bloom;
 
+    /// <summary>
+    /// 降采样次数
+    /// </summary>
+    [Range(1, 16)]public int iterations = 4;
+
     [Range(0, 10)]public float intensity = 1;
 
+    /// <summary>
+    /// 能产生bloom效果的亮度的阈值
+    /// </summary>
     [Range(0, 10)]public float threshold = 1;
 
+    /// <summary>
+    /// 这个值可以认为用来调整threshold的百分比
+    /// </summary>
+    [Range(0, 1)]public float softThreshold = 0.5f;
+
+    //这个用来存储降采样时的中间图，用来blend one one到升采样的中间图中
+    //之所以是16是16个数能代表65535，一张图缩放一倍，那么16张图可以缩65535倍，实际根本用不到这么多
+    //demo只缩3次就是1/8
+    RenderTexture[] textures = new RenderTexture[16];
+
+    /// <summary>
+    /// 开启就只输出最终的模糊图，而不输出模糊图叠加到原始图上的效果
+    /// </summary>
+    public bool debug;
 
     void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
@@ -33,23 +56,76 @@ public class BloomPostEffect : MonoBehaviour
             bloom.hideFlags = HideFlags.HideAndDontSave;
         }
 
+
+        float knee = threshold * softThreshold;
+        Vector4 filter;
+
+        //除了x外，其他3个参数都配合公式，因为具体像素计算在shader中，这里尽可能把能提前在cpu中算好的部分算好，然后再传入gpu
+        //像素最大亮度相关的值只能是针对单个像素在gpu中计算，所以这里不能把所有东西都算好再传入gpu
+        filter.x = threshold;//阈值
+        filter.y = filter.x - knee;
+        filter.z = 2f * knee;
+        filter.w = 0.25f / (knee + 0.00001f);
+        bloom.SetVector("_Filter", filter);       
+        //nafio info这里把强度从gamma转到线性要注意下
         bloom.SetFloat("_Intensity", Mathf.GammaToLinearSpace(intensity));
 
+
+        int width = source.width / 2;
+        int height = source.height / 2;
         RenderTextureFormat format = source.format;
 
-        int width = (source.width >> 1);
+        textures[0] = RenderTexture.GetTemporary(width, height, 0, format);
+        RenderTexture currentDestination = textures[0];
+        Graphics.Blit(source, currentDestination, bloom, BoxDownPrefilterPass);
+        RenderTexture currentSource = currentDestination;
 
-        int height = (source.height >> 1);
+        int i = 1;
+        //降采样，注意在这之前已经执行了第0次降采样，所以这里从1开始
+        for (i=1; i < iterations; i++)
+        {
+            width = (width>>1);
 
-        RenderTexture currentDestination = RenderTexture.GetTemporary(width, height, 0, format);
+            height= (height>>1);
 
-        Graphics.Blit(source, currentDestination, bloom, 0);
+            if (height < 2)
+            {
+                //这里代表缩放次数过多，都已经缩放到低于2像素了，没有意义了
+                Debug.LogError("降采样 height:"+height);
+                break;
+            }
 
-        bloom.SetTexture("_SourceTex", source);
-        Graphics.Blit(currentDestination, destination, bloom, 1);
+            textures[i] = RenderTexture.GetTemporary(width, height, 0, format);
+            currentDestination = textures[i];
+            Graphics.Blit(currentSource, currentDestination, bloom, BoxDownPass);
+            currentSource = currentDestination;
+        }
+
+        // 升采样，注意在循环最后一定会再执行一次渲染到相机的操作，所以这里i -= 2
+        //假设降采样迭代2次，那么这里循环就执行1次，最后还会通过Graphics.Blit(currentSource, destination, bloom, ApplyBloomPass);
+        //再执行一次，那么升降次数就相同了
+        for (i -= 2; i >= 0; i--)
+        {
+            currentDestination = textures[i];
+            textures[i] = null;
+            Graphics.Blit(currentSource, currentDestination, bloom, BoxUpPass);
+            RenderTexture.ReleaseTemporary(currentSource);
+            currentSource = currentDestination;
+        }
+
+        if (debug)
+        {
+            Graphics.Blit(currentSource, destination, bloom, DebugBloomPass);
+        }
+        else
+        {
+            bloom.SetTexture("_SourceTex", source);
+            Graphics.Blit(currentSource, destination, bloom, ApplyBloomPass);
+        }
+        RenderTexture.ReleaseTemporary(currentSource);
 
 
-        RenderTexture.ReleaseTemporary(currentDestination);
+
 
     }
 
--- Assets/shader/postEffect/BloomPostEffect/BloomPostEffect.shader
+++ Assets/shader/postEffect/BloomPostEffect/BloomPostEffect.shader
@@ -2,6 +2,7 @@
 {
     Properties
     {
+		//nafio info 这个图虽然没有具体图片但也是必要的，用来取单个图像素大小
         _MainTex ("Texture", 2D) = "white" {}
     }
 
@@ -11,6 +12,11 @@
     sampler2D _SourceTex;
     sampler2D _MainTex;
     float4 _MainTex_TexelSize;
+
+	//用于传入softThread 公式需要使用的一些参数
+	half4 _Filter;
+
+	half _Intensity;
 
     struct a2v
     {
@@ -48,6 +54,23 @@
         return s * 0.25f;
     }
 
+	//ninfo
+	//这个函数的目的是
+	//1 找出亮度超出阈值BloomEffect.threshold的部分，超出越多模糊越大
+	//2 BloomEffect.softThreshold的作用是修正BloomEffect.threshold，使原来陡峭的过渡曲线变得平滑
+	//3 Prefilter计算亮度对最终bloom效果的影响contribution，只需要在第一次降采样时计算就可以，不需要多次计算
+	//https://catlikecoding.com/unity/tutorials/advanced-rendering/bloom/
+	half3 Prefilter (half3 c)
+	{
+		half brightness = max(c.r, max(c.g, c.b));
+		half soft = brightness - _Filter.y;
+		soft = clamp(soft, 0, _Filter.z);
+		soft = soft * soft * _Filter.w;
+		half contribution = max(soft, brightness - _Filter.x);
+		contribution /= max(brightness, 0.00001);		
+		return c * contribution;
+	}
+
     ENDCG
 
     SubShader
@@ -56,22 +79,33 @@
         ZTest Always
         ZWrite Off
 
-        Pass { // 0 首次降采样后，处理高亮阈值
+		Pass { // 0
+			CGPROGRAM
+				#pragma vertex vert
+                #pragma fragment frag
+
+				half4 frag (v2f i) : SV_Target
+				{
+					return half4(Prefilter(SampleBox(i.uv, 1)), 1);
+				}
+			ENDCG
+		}
+
+        Pass { // 1 非首次降采样
             CGPROGRAM
                 #pragma vertex vert
                 #pragma fragment frag
                 
                 half4 frag(v2f i) : SV_Target
                 {
-                    //return half4(1,1,1,1);
                     return half4(SampleBox(i.uv, 1), 1);
                 }
             ENDCG
         }
 
-        Pass { // 2 升采样
+        Pass { // 2 非最后一次升采样
 
-            //Blend One One
+            Blend One One
 
             CGPROGRAM
                 #pragma vertex vert
@@ -81,7 +115,34 @@
                 {
                     return half4(SampleBox(i.uv, 0.5), 1);
                 }
-        ENDCG
-    }
+			ENDCG
+		}
+
+		Pass { // 3
+			CGPROGRAM
+				#pragma vertex vert
+                #pragma fragment frag
+
+				half4 frag (v2f i) : SV_Target {
+					half4 c = tex2D(_SourceTex, i.uv);
+					c.rgb += _Intensity * SampleBox(i.uv, 0.5);
+					return c;
+				}
+			ENDCG
+		}
+
+		//ninfo 这个pass只有debug开启后才用，用来查看bloom生效最明显的部分
+		//就是输出与原始图片叠加前，最后计算出的bloom效果
+		Pass { // 4 最后一次升采样，也是模糊图与原图
+			CGPROGRAM
+				#pragma vertex vert
+                #pragma fragment frag
+
+				half4 frag (v2f i) : SV_Target {
+					return half4(_Intensity * SampleBox(i.uv, 0.5), 1);
+				}
+			ENDCG
+		}
+
     }
 }
